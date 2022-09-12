de8bb06bcf66df1897cc35a114d29d996ff5bf9d
cesar <cesardyno0207-@gmail.com>
2022-09-12 12:12:14 -0500

    creacion de boton

--- index.py
+++ index.py
@@ -10,6 +10,24 @@ class nomina:
         self.wind = window
         self.wind.title('Nomina generate')
 
+        #crear contenedor
+        frame = LabelFrame(self.wind, text = 'register a new nomina')
+        frame.grid(row = 0, column= 0, columnspan= 3, pady=20)
+
+        #name input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.grid(row = 1, column= 1)
+
+        #salary input
+        Label(frame, text='Salary: ').grid(row= 2, column= 0)
+        self.salary = Entry(frame)
+        self.salary.grid(row=2, column=1)
+
+        #add button
+        ttk.Button(frame, text = 'Generate nomina').grid(row=3,columnspan=2, sticky= W + E)
+
+
 if __name__ == '__main__':
     window = Tk()
     application = nomina(window)
