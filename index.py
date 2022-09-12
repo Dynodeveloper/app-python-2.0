4a384123deaaa1ff42d3a08ddb581f6c1fff08e2
cesar <cesardyno0207-@gmail.com>
2022-09-12 12:41:04 -0500

    coneccion y obtencion de datos

--- index.py
+++ index.py
@@ -6,6 +6,8 @@ import sqlite3
 
 class nomina:
 
+    db_name = 'database.db'
+
     def __init__(self,window): 
         self.wind = window
         self.wind.title('Nomina generate')
@@ -38,7 +40,29 @@ class nomina:
         self.tree.grid(row=5, column=0, columnspan= 2)
         self.tree.heading('#0', text='Name', anchor=CENTER)
         self.tree.heading('#1',text='Salary', anchor=CENTER)
-    
+
+        self.get_nomina()
+
+    def run_query(self, query,parameters=()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    def get_nomina(self):
+        
+        #delet records
+        records=self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        #consulta    
+
+        query = 'SELECT * FROM Nomina ORDER BY Name DESC'       
+        db_rows=self.run_query(query)
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
 
 
 if __name__ == '__main__':
